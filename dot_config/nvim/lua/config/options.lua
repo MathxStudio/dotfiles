21bc7e3b87786c6ad0ed1050d04357f37bbe4bd7
Allan Pan <mathxstudio@gmail.com>
2026-04-10 17:16:35 +0800

    latest update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.breakindent = true
+vim.opt.shell = "/bin/zsh"
+vim.env.http_proxy = "http://127.0.0.1:7890"
+vim.env.https_proxy = "http://127.0.0.1:7890"
+vim.env.all_proxy = "socks5://127.0.0.1:7890"
